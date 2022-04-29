aa17339cfdba7528433be86c7396b19769e8d993
Jeevanantham <ajaybharathik@gmail.com>
2022-04-29 11:29:15 +0530

    Migrations Added

--- Source/A5/Migrations/20220429055057_InitialChanges.Designer.cs
+++ Source/A5/Migrations/20220429055057_InitialChanges.Designer.cs
@@ -3,6 +3,7 @@ using A5.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace A5.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220429055057_InitialChanges")]
+    partial class InitialChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Source/A5/Migrations/20220429055057_InitialChanges.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace A5.Migrations
+{
+    public partial class InitialChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OrganisationId",
+                table: "Organisations",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "DesignationId",
+                table: "Designations",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "DepartmentId",
+                table: "Departments",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Organisations",
+                newName: "OrganisationId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Designations",
+                newName: "DesignationId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Departments",
+                newName: "DepartmentId");
+        }
+    }
+}
